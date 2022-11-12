de362ab9305882f8a54a273d2475946173e4e43b
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2022-11-12 17:26:49 +0900

    포트폴리오 완성

--- next-portfolio/components/section/PageSkills.js
+++ next-portfolio/components/section/PageSkills.js
@@ -26,34 +26,54 @@ function PageSkills() {
   return (
     <div className="text-center my-32">
       <div>
-        <h1 className="text-4xl mb-4 font-black text-gray-900">Skills</h1>
+        <h1 className="text-4xl mb-4 font-semibold text-gray-900">Skills</h1>
       </div>
       <div className="flex">
         <div className="w-3/5 skill-card ">
-          <div className="flex flex-col flex-wrap flex-around">
-            <div className="mb-8 flex justify-around">
-              <Image src={html} width={100} height={150} />
-              <Image src={css} width={100} height={150} />
-              <Image src={js} width={100} height={150} />
-            </div>
-            <div className="mb-8 flex justify-around">
-              <Image src={jquery} width={120} height={150} />
-              <Image src={bootstrap} width={150} height={150} />
-              <Image src={tailwindcss} width={220} height={80} />
-            </div>
-            <div className="mb-12 flex justify-around">
-              <Image src={axios} width={200} height={30} />
-              <Image src={react} width={200} height={50} />
-            </div>
-            <div className="mb-8 flex justify-around">
-              <Image src={redux} width={100} height={150} />
-              <Image src={reduxsaga} width={150} height={150} />
-              <Image src={nextjs} width={150} height={150} />
-            </div>
-            <div className="flex justify-center">
-              <div className="text-center rounded-2xl bg-blue-500 text-white text-xl font-bold w-[150px] h-[50px] flex items-center">
-                &emsp;&nbsp;Front-End
+          <div className="flex flex-col   items-center">
+            <div className="mb-12 flex space-x-12">
+              <div>
+                <Image src={html} width={80} height={120} />
               </div>
+              <div>
+                <Image src={css} width={80} height={120} />
+              </div>
+              <div>
+                <Image src={js} width={80} height={120} />
+              </div>
+            </div>
+            <div className="mb-12 flex space-x-12">
+              <div>
+                <Image src={jquery} width={100} height={120} />
+              </div>
+              <div>
+                <Image src={bootstrap} width={120} height={120} />
+              </div>
+              <div>
+                <Image src={tailwindcss} width={220} height={120} />
+              </div>
+            </div>
+            <div className="mb-12 flex space-x-12">
+              <div>
+                <Image src={axios} width={200} height={30} />
+              </div>
+              <div>
+                <Image src={react} width={200} height={50} />
+              </div>
+            </div>
+            <div className="mb-8 flex space-x-12">
+              <div>
+                <Image src={redux} width={100} height={120} />
+              </div>
+              <div>
+                <Image src={reduxsaga} width={120} height={130} />
+              </div>
+              <div>
+                <Image src={nextjs} width={200} height={150} />
+              </div>
+            </div>
+            <div className="text-center rounded-2xl bg-blue-500 text-white text-xl font-bold w-[150px] h-[50px] flex justify-center items-center">
+              Front-End
             </div>
           </div>
         </div>
@@ -71,13 +91,13 @@ function PageSkills() {
             <div className="mb-8">
               <Image src={mysql} />
             </div>
-            <div className="ml-2 text-center rounded-2xl bg-blue-500 text-white text-xl font-bold w-[150px] h-[50px] flex items-center">
-              &emsp;&nbsp;&nbsp;Back-End
+            <div className="ml-2 text-center rounded-2xl bg-blue-500 text-white text-xl font-bold w-[150px] h-[50px] flex justify-center items-center">
+              Back-End
             </div>
           </div>
         </div>
-        <div className="w-1/5 skill-card items-center">
-          <div className="flex flex-col justify-around items-center">
+        <div className="w-1/5 skill-card">
+          <div className="flex flex-col ">
             <div className="my-16">
               <Image src={github} width={100} height={100} />
             </div>
@@ -87,8 +107,8 @@ function PageSkills() {
             <div className="my-16">
               <Image src={sourcetree} />
             </div>
-            <div className="ml-2 mt-8 text-center rounded-2xl bg-blue-500 text-white text-xl font-bold w-[150px] h-[50px] flex items-center">
-              &emsp;&emsp;&nbsp;&nbsp;&nbsp;ETC
+            <div className="ml-2 mt-8 text-center rounded-2xl bg-blue-500 text-white text-xl font-bold w-[150px] h-[50px] flex justify-center items-center">
+              ETC
             </div>
           </div>
         </div>
